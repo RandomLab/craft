96bd8faccb7214b0c62b50df87fdfc4e724d15d4
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2016-06-22 16:13:48 +0200

    Logo + Score pour un type

--- run.py
+++ run.py
@@ -2,35 +2,46 @@ from Robot import Robot
 import tkinter as tk
 import time
 import random
+from UI.List import *
+from Robot.classes import *
 # Mise à jour du robit (en secondes)
 ROBOT_UPDATE_TIME = 5
 WIDTH = 200
 HEIGHT = 500
 
+player = os.getenv("USERNAME")
 
 class App():
     def __init__(self):
         self.robot = Robot.Robot(secondes=ROBOT_UPDATE_TIME)
         self.root = tk.Tk()
-        #self.label = tk.Label(text="")
-        #self.label.pack()
-        self.canvas = tk.Canvas(width = WIDTH, height = HEIGHT)
-        self.canvas.pack()
+        #self.canvas = tk.Canvas(width = WIDTH, height = HEIGHT)
+        #self.canvas.pack()
+        i = tk.PhotoImage(file = "ressources/Ingenieur.png")
+        logo = tk.Label(image = i)
+        logo.image = i
+        logo.pack(side="top")
+
+        self.label = List(player, 0)
+        self.label.pack()
         self.update_clock()
         self.update_robot()
         self.root.mainloop()
 
     def update_clock(self):
         now = time.strftime("%H:%M:%S")
-        #self.label.configure(text=now)
-        self.canvas.create_text(random.randrange(WIDTH), random.randrange(HEIGHT), text = "Bonjour")
+        #self.canvas.create_text(random.randrange(WIDTH), random.randrange(HEIGHT), text = "Bonjour")
         self.root.after(1000, self.update_clock)
 
 
     def update_robot(self):
         print("update robot")
         self.robot.run()
-        print(Robot.items)
+        print(len(Robot.find(Arme)))
+
+        self.label.configure(score = len(Robot.find(Arme)))
+
+
         self.root.after(self.robot.secondes * 1000, self.update_robot)
 
 
