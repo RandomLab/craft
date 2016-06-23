93dec36ea37bf96d495bb599aff93068f9c8f3e7
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2016-06-23 18:05:50 +0200

    Comm ok

--- run.py
+++ run.py
@@ -6,7 +6,12 @@ from UI.List import *
 from Robot.classes import *
 from pythonosc import dispatcher
 from pythonosc import osc_server
+from pythonosc import osc_message_builder
+
+from pythonosc import udp_client
+
 import threading
+import socket
 # Mise à jour du robit (en secondes)
 ROBOT_UPDATE_TIME = 5
 WIDTH = 200
@@ -16,7 +21,28 @@ player = os.getenv("USERNAME")
 
 class App():
     def __init__(self):
+        self.registered = False
         self.robot = Robot.Robot(secondes=ROBOT_UPDATE_TIME)
+        #self.update_robot()
+        self.dispatcher = dispatcher.Dispatcher()
+        self.dispatcher.map("/clock", self.update_robot, "TEST")
+        self.dispatcher.map("/hack", self.server_hack, "Hack")
+
+
+        self.server = osc_server.ThreadingOSCUDPServer(("0.0.0.0", 5005), self.dispatcher)
+        self.server_thread = threading.Thread(target=self.server.serve_forever)
+        self.server_thread.start()
+        self.client = udp_client.UDPClient("255.255.255.255", 5006)
+        if hasattr(socket,'SO_BROADCAST'):
+            self.client._sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+        print("Registering to server...")
+        msg = osc_message_builder.OscMessageBuilder(address="/register")
+        msg.add_arg(player)
+        msg.add_arg("172.17.24.225") #
+        msg = msg.build()
+        self.client.send(msg)
+        print("Waiting for server")
         self.root = tk.Tk()
 
         self.root.geometry('{}x{}'.format(200, 500))
@@ -30,17 +56,11 @@ class App():
 
         self.label = List(player, 0)
         self.label.pack()
-        #self.update_robot()
-        self.dispatcher = dispatcher.Dispatcher()
-        self.dispatcher.map("/clock", self.update_robot, "TEST")
 
-        self.server = osc_server.ForkingOSCUDPServer(("0.0.0.0", 5005), self.dispatcher)
-        self.server_thread = threading.Thread(target=self.server.serve_forever)
-        self.server_thread.start()
-
-        #self.server = osc_server.ThreadingOSCUDPServer(("0.0.0.0", 5005), dispatcher)
-        #self.update_clock()
         self.root.mainloop()
+
+    def server_hack(self, unused_addr, args):
+        print("Got hack from server")
     def on_closing(self):
         print("Closing")
         self.server.shutdown()
