dd3a94f53efa53eecb97da11735e302ef2f78a07
Samuel Hackwill <samuel@tu-vas-y-arriver.local>
2016-06-28 14:44:16 +0200

    Resolve last confict after merge

--- config.py
+++ config.py
@@ -1,10 +1,2 @@
 player_name = "samuel"
 base_path = "/Users/samuel/Desktop"
-
-<<<<<<< HEAD
-base_path = "/Users/samuel/Desktop"
-=======
-
->>>>>>> Network
-#base_path = "/home/jdpillon/Bureau/samuel"
-#base_path = "/Users/jdpillon/Desktop/samuel/Desktop"
